1aa8cad4f6662d2e77068115236b49b57efdc8aa
jhpark <hareguu89@gmail.com>
2022-02-17 18:48:35 +0900

    Update Todo List

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -6,106 +6,200 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BBANTO',
-      home: Grade(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      title: 'Pracatice App',
+      home: TodoList(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
+class Todo {
+  bool isDone = false;
+  String title;
+
+  Todo(this.title);
+}
+
+class TodoList extends StatefulWidget {
   @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  final _items = <Todo>[];
+  var _todoController = TextEditingController();
+
+  @override
+  void dispose() {
+    _todoController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
       appBar: AppBar(
-        title: Text('뭐야'),
-        backgroundColor: Colors.amber[700],
-        centerTitle: true,
-        elevation: 0.0,
+        title: Text('남은 할일'),
       ),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(40.0, 30.0, 0.0, 0.0),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'NAME',
-              style: TextStyle(color: Colors.white, letterSpacing: 2.0),
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: TextField(
+                  controller: _todoController,
+                )),
+                ElevatedButton(
+                    onPressed: () {
+                      _addTodo(Todo(_todoController.text));
+                    },
+                    child: Text('추가'))
+              ],
             ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'JH',
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              'JH POWER LEVEL',
-              style: TextStyle(color: Colors.white, letterSpacing: 2.0),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              '14',
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(children: <Widget>[
-              Icon(Icons.check_circle_outline),
-              SizedBox(
-                width: 10.0,
-              ),
-              Text(
-                'using lightsaber',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0,
-                ),
-              )
-            ]),
-            Row(children: <Widget>[
-              Icon(Icons.check_circle_outline),
-              SizedBox(
-                width: 10.0,
-              ),
-              Text(
-                'face hero tattoo',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0,
-                ),
-              )
-            ]),
-            Row(children: <Widget>[
-              Icon(Icons.check_circle_outline),
-              SizedBox(
-                width: 10.0,
-              ),
-              Text(
-                'fire flames',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0,
-                ),
-              )
-            ])
+            Expanded(
+                child: ListView(
+              children: _items.map((data) => _buildItemWidget(data)).toList(),
+            ))
           ],
         ),
       ),
     );
   }
+
+  Widget _buildItemWidget(Todo todo) {
+    return ListTile(
+      onTap: () => _toggleTodo(todo),
+      title: Text(todo.title,
+          style: todo.isDone
+              ? TextStyle(
+                  decoration: TextDecoration.lineThrough,
+                  fontStyle: FontStyle.italic)
+              : null),
+      trailing: IconButton(
+        icon: Icon(Icons.delete_forever),
+        onPressed: () => _deleteTodo(todo),
+      ),
+    );
+  }
+
+  void _addTodo(Todo todo) {
+    setState(() {
+      _items.add(todo);
+      _todoController.text = "";
+    });
+  }
+
+  void _deleteTodo(Todo todo) {
+    setState(() {
+      _items.remove(todo);
+      print(_items);
+    });
+  }
+
+  void _toggleTodo(Todo todo) {
+    todo.isDone = !todo.isDone;
+  }
 }
+
+// class Grade extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         backgroundColor: Colors.amber,
+//         appBar: AppBar(
+//           title: Text('메뉴!!'),
+//           backgroundColor: Colors.amber[700],
+//           centerTitle: true,
+//           elevation: 0.0,
+//         ),
+//         body: Padding(
+//           padding: EdgeInsets.fromLTRB(40.0, 30.0, 0.0, 0.0),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: <Widget>[
+//               Text(
+//                 '이름',
+//                 style: TextStyle(color: Colors.white, letterSpacing: 2.0),
+//               ),
+//               SizedBox(
+//                 height: 10.0,
+//               ),
+//               Text(
+//                 '박정환',
+//                 style: TextStyle(
+//                     color: Colors.white,
+//                     letterSpacing: 2.0,
+//                     fontSize: 28.0,
+//                     fontWeight: FontWeight.bold),
+//               ),
+//               SizedBox(
+//                 height: 30.0,
+//               ),
+//               Text(
+//                 '가능스택',
+//                 style: TextStyle(color: Colors.white, letterSpacing: 2.0),
+//               ),
+//               SizedBox(
+//                 height: 10.0,
+//               ),
+//               Text(
+//                 '개많음!!',
+//                 style: TextStyle(
+//                     color: Colors.white,
+//                     letterSpacing: 2.0,
+//                     fontSize: 28.0,
+//                     fontWeight: FontWeight.bold),
+//               ),
+//               SizedBox(
+//                 height: 30.0,
+//               ),
+//               Row(children: <Widget>[
+//                 Icon(Icons.check_circle_outline),
+//                 SizedBox(
+//                   width: 10.0,
+//                 ),
+//                 Text(
+//                   'react',
+//                   style: TextStyle(
+//                     fontSize: 16.0,
+//                     letterSpacing: 1.0,
+//                   ),
+//                 )
+//               ]),
+//               Row(children: <Widget>[
+//                 Icon(Icons.check_circle_outline),
+//                 SizedBox(
+//                   width: 10.0,
+//                 ),
+//                 Text(
+//                   'vue',
+//                   style: TextStyle(
+//                     fontSize: 16.0,
+//                     letterSpacing: 1.0,
+//                   ),
+//                 )
+//               ]),
+//               Row(children: <Widget>[
+//                 Icon(Icons.check_circle_outline),
+//                 SizedBox(
+//                   width: 10.0,
+//                 ),
+//                 Text(
+//                   'flutter',
+//                   style: TextStyle(
+//                     fontSize: 16.0,
+//                     letterSpacing: 1.0,
+//                   ),
+//                 )
+//               ])
+//             ],
+//           ),
+//         ),
+//         bottomNavigationBar: Padding(
+//           padding: EdgeInsets.fromLTRB(40, 0, 30, 40),
+//           child: BottomAppBar(
+//               child: Text('Rock Bottom'), color: Colors.orangeAccent),
+//         ));
+//   }
+// }
